445de2916098421f9850695feb910e74b7d85af2
sunil kumar samal <sunilsamal322@gmail.com>
2022-06-01 23:10:05 +0530

    [Add]Added program for find greater number

---
+++ src/31-May-2022/Q8GreatestNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Q8GreatestNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        int greater_num=num1>num2 && num1>num3?num1:num2>num3?num2:num3;
+        System.out.println("The greater number between "+num1+","+num2+" and "+num3+" is "+greater_num);
+    }
+}
