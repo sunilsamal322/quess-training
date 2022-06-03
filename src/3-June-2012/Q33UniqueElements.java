08a95d10056cfc305e2ed92c9659209c9b5ad56d
sunil kumar samal <sunilsamal322@gmail.com>
2022-06-03 22:05:21 +0530

    [Add]Added program for find no of unique elements in array

---
+++ src/3-June-2012/Q33UniqueElements.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q33UniqueElements {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter array size");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter elements for array");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        ArrayList<Integer> al=new ArrayList<>();
+        for(int i=0;i<size;i++)
+        {
+            for(int j=i+1;j<size;j++)
+            {
+                if(arr[i]==arr[j]) {
+                    if(!al.contains(arr[i])){
+                        al.add(arr[i]);
+                    }
+                }
+            }
+        }
+        int duplicateCount=-al.size();
+        for(int a:al)
+        {
+            for(int i=0;i<arr.length;i++)
+            {
+                if(a==arr[i])
+                {
+                    duplicateCount++;
+                }
+            }
+        }
+        System.out.println("No of unique elements are "+(size-duplicateCount));
+    }
+}
