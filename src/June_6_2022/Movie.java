fe95f716b288ec2fc4c887f3d0e4590772e7c29e
sunil kumar samal <sunilsamal322@gmail.com>
2022-06-06 16:30:23 +0530

    [Add]adding functionality for constructor

---
+++ src/June_6_2022/Movie.java
@@ -0,0 +1,28 @@
+package June_6_2022;
+
+import java.util.Scanner;
+
+public class Movie {
+    private String title;
+    private String studio;
+    private String rating;
+
+    Movie(String title,String studio,String rating)
+    {
+        this.title=title;
+        this.studio=studio;
+        this.rating=rating;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter title");
+        String title=scan.next();
+        System.out.println("Enter studio name");
+        String studio= scan.next();
+        System.out.println("Enter ratings");
+        String rating=scan.next();
+        Movie film=new Movie(title,studio,rating);
+
+    }
+}
